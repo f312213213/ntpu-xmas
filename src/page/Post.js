47da3fa138e1a659343ff110067104319ec76efc
david <f312213213david@gmail.com>
2021-12-19 15:20:49 +0800

    Add loading effect

--- src/page/Post.js
+++ src/page/Post.js
@@ -1,21 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { getDatabase, ref, set } from "firebase/database";
 import {Navigate} from "react-router-dom";
+import Loader from "react-loader-spinner";
 
 const Post = ({user}) => {
-    function _uuid() {
-        function s4() {
-            return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-        }
-        return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
-    }
+    const [loading, setLoading] = useState(false);
 
     const addPost = () => {
+        setLoading(true)
         if (idRef.current.value === '' || textRef.current.value === ''){
             alert('兩個都要填喔！')
+            setLoading(false)
         }
         else if(idRef.current.value.length !== 9){
             alert('學號不正確喔！')
+            setLoading(false)
         }
         else{
             const database = getDatabase();
@@ -26,6 +25,7 @@ const Post = ({user}) => {
             idRef.current.value = ''
             textRef.current.value = ''
             alert('我們收到囉，會在聖誕節當天幫你寄出這封信！')
+            setLoading(false)
         }
     }
 
@@ -60,7 +60,14 @@ const Post = ({user}) => {
                     </h1>
                     <input type="text" ref={idRef} onChange={detectLetter} className={'ring-2 focus:ring-4 p-2 rounded-xl border border-rose-400'} maxLength={9} placeholder={'對方的學號'}/>
                     <textarea name="" ref={textRef} className={'ring-2 focus:ring-4 p-2 rounded-xl p-2 border border-rose-400'} placeholder={'想說的話！'} id="" cols="30" rows="10"></textarea>
-                    <button onClick={addPost} className={'border-2 rounded-md p-2'}>送出</button>
+                    <button onClick={addPost} className={'border-2 rounded-md p-2'}>
+                        {
+                            loading ?
+                                <Loader type="ThreeDots" color="#000" height={25} width={25} />
+                                :
+                                '送出'
+                        }
+                    </button>
                     <p className={'text-black'}>
                         對方會在聖誕節當天在他的學術信箱裡收到你留給他的話唷！
                     </p>
--- src/page/Signin.js
+++ src/page/Signin.js
@@ -1,27 +1,31 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import { GoogleAuthProvider, getAuth, signInWithRedirect, getRedirectResult } from "firebase/auth";
 import {Navigate} from "react-router-dom";
+import Loader from "react-loader-spinner";
 
 const Signin = ({setUser, user}) => {
     const provider = new GoogleAuthProvider();
     const auth = getAuth();
+    const [loading, setLoading] = useState(false);
     useEffect(()=>{
+        setLoading(true)
         getRedirectResult(auth)
             .then((result) => {
                 const credential = GoogleAuthProvider.credentialFromResult(result);
                 const token = credential.accessToken;
                 const userGet = result.user;
                 setUser(userGet)
+                setLoading(false)
             }).catch((error) => {
             const errorCode = error.code;
             const errorMessage = error.message;
             const email = error.email;
             const credential = GoogleAuthProvider.credentialFromError(error);
+            setLoading(false)
         });
-
-
     },[])
     const signinButton = () => {
+        setLoading(true)
         signInWithRedirect(auth, provider)
     }
     if (user.email) {
@@ -31,7 +35,12 @@ const Signin = ({setUser, user}) => {
         return (
             <div className={'flex justify-center items-center bg-rose-500 min-h-screen w-full flex-col'}>
                 <button onClick={signinButton} className={'border-2 p-2 rounded-md bg-white'}>
-                    登入 - 請使用學校配發之學術信箱
+                    {
+                        loading ?
+                            <Loader type="ThreeDots" color="#000" height={25} width={25} />
+                            :
+                            '登入-請使用學校配發之學術信箱'
+                    }
                 </button>
                 <p>
                     對方不會收到任何關於你的資訊，實名制僅確保使用者不會濫用此服務。
